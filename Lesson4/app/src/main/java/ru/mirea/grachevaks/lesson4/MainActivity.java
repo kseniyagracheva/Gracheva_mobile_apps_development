9b971bd671fb6355d19f7d77a64ec3ac1210755a
kseniyagracheva <Grachevakseniya17@yandex.ru>
2025-05-04 23:38:33 +0300

    Finish music_player

--- Lesson4/app/src/main/java/ru/mirea/grachevaks/lesson4/MainActivity.java
+++ Lesson4/app/src/main/java/ru/mirea/grachevaks/lesson4/MainActivity.java
@@ -1,24 +1,65 @@
 package ru.mirea.grachevaks.lesson4;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import ru.mirea.grachevaks.lesson4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MediaPlayer mediaPlayer;
+    private boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.songTitle.setText("More More More");
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.more_more_more);
+
+        binding.playPauseButton.setOnClickListener(v -> {
+            if (mediaPlayer == null) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.more_more_more);
+                isPlaying = true;
+                binding.playPauseButton.setText("Пауза");
+                // mediaPlayer.start();
+            } else if (isPlaying) {
+                isPlaying = false;
+                binding.playPauseButton.setText("Играть");
+                mediaPlayer.pause();
+            } else {
+                isPlaying = true;
+                binding.playPauseButton.setText("Пауза");
+                mediaPlayer.start();
+            }
         });
+
+        binding.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {}
+            @Override public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.seekTo(seekBar.getProgress());
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
